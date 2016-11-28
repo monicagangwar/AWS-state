caa35c9acf42b600f4d6cda81778b409f1d8a646
monica-gangwar <monica_gangwar@mckinsey.com>
2016-11-28 15:00:31 +0530

    Reporting errors while pushing the code

--- terraforming.py
+++ terraforming.py
@@ -5,7 +5,6 @@ import datetime
 
 global access_key
 global secret_key
-#global session_token 
 
 def run_cmd(args):
   p = sp.Popen(args,stdout=sp.PIPE,stderr=sp.PIPE)
@@ -40,41 +39,43 @@ regions = ['us-east-1','us-west-1','us-west-2','eu-west-1','eu-central-1','ap-no
 same_in_all_region = ['iamp','iamg','iamgp','iamgp','iamr','iamrp','iamu','iamip','iamup']
 services = {}
 services['AutoScaling Group']          =  'asg'
-#services['Database Parameter Group']   =  'dbpg'
-#services['Database Security Group']    =  'dbsg'
-#services['Database Subnet Group']      =  'dbsn'
-#services['EC2']                        =  'ec2'
-#services['ElastiCache Cluster']        =  'ecc'
-#services['ElastiCache Subnet Group']   =  'ecsn'
-#services['EIP']                        =  'eip'
-#services['ELB']                        =  'elb'
-#services['IAM Group']                  =  'iamg'
-#services['IAM Group Membership']       =  'iamgm'
-#services['IAM Group Policy']           =  'iamgp'
-#services['IAM Instance Profile']       =  'iamip'
-#services['IAM Policy']                 =  'iamp'
-#services['IAM Role']                   =  'iamr'
-#services['IAM Role Policy']            =  'iamrp'
-#services['IAM User']                   =  'iamu'
-#services['IAM User Policy']            =  'iamup'
-#services['Internet Gateway']           =  'igw'
-#services['Launch Configuration']       =  'lc'
-#services['Network ACL']                =  'nacl'
-#services['Network Interface']          =  'nif'
-#services['Route53 Record']             =  'r53r'
-#services['Route53 Hosted Zone']        =  'r53z'
-#services['RDS']                        =  'rds'
-#services['Redshift']                   =  'rs' 
-#services['Route Table']                =  'rt'
-#services['Route Table Association']    =  'rta'
-#services['S3']                         =  's3'
-#services['Security Group']             =  'sg'
-#services['Subnet']                     =  'sn'
-#services['SQS']                        =  'sqs'
-#services['VPN Gateway']                =  'vgw'
-#services['VPC']                        =  'vpc'
+services['Database Parameter Group']   =  'dbpg'
+services['Database Security Group']    =  'dbsg'
+services['Database Subnet Group']      =  'dbsn'
+services['EC2']                        =  'ec2'
+services['ElastiCache Cluster']        =  'ecc'
+services['ElastiCache Subnet Group']   =  'ecsn'
+services['EIP']                        =  'eip'
+services['ELB']                        =  'elb'
+services['IAM Group']                  =  'iamg'
+services['IAM Group Membership']       =  'iamgm'
+services['IAM Group Policy']           =  'iamgp'
+services['IAM Instance Profile']       =  'iamip'
+services['IAM Policy']                 =  'iamp'
+services['IAM Role']                   =  'iamr'
+services['IAM Role Policy']            =  'iamrp'
+services['IAM User']                   =  'iamu'
+services['IAM User Policy']            =  'iamup'
+services['Internet Gateway']           =  'igw'
+services['Launch Configuration']       =  'lc'
+services['Network ACL']                =  'nacl'
+services['Network Interface']          =  'nif'
+services['Route53 Record']             =  'r53r'
+services['Route53 Hosted Zone']        =  'r53z'
+services['RDS']                        =  'rds'
+services['Redshift']                   =  'rs' 
+services['Route Table']                =  'rt'
+services['Route Table Association']    =  'rta'
+services['S3']                         =  's3'
+services['Security Group']             =  'sg'
+services['Subnet']                     =  'sn'
+services['SQS']                        =  'sqs'
+services['VPN Gateway']                =  'vgw'
+services['VPC']                        =  'vpc'
 
-root_dir = os.getcwd() + '/infra'
+dir_name = 'Infra'
+cur_dir = os.getcwd()
+root_dir = cur_dir + '/' + dir_name
 if os.path.exists(root_dir) == False:
   os.mkdir(root_dir)
 os.chdir(root_dir)
@@ -87,22 +88,27 @@ for service,service_id in services.iteritems():
       f.write(out)
       f.close()
   else:
-    cur_dir = root_dir + '/' + service
-    if os.path.exists(cur_dir) == False:
-      os.mkdir(cur_dir)
-    os.chdir(cur_dir)
+    service_dir = root_dir + '/' + service
+    if os.path.exists(service_dir) == False:
+      os.mkdir(service_dir)
+    os.chdir(service_dir)
     for region in regions:
       print '.',
       out,err = run_cmd(['docker' , 'run' , '--rm' , '--name' , 'terraforming' , '-e' , 'AWS_ACCESS_KEY_ID='+access_key , '-e' , 'AWS_SECRET_ACCESS_KEY='+secret_key , '-e' , 'AWS_REGION='+region , 'quay.io/dtan4/terraforming:latest' , 'terraforming' , service_id])
       if out.isspace() == False:
-        f = open(cur_dir + '/' + region, 'w')
+        f = open(service_dir + '/' + region, 'w')
         f.write(out)
         f.close()
     print "\n"
   os.chdir(root_dir)
 
-run_cmd[('git','add','--all')]
-commit_message = '\'Infra at ' + datetime.datetime.now() + '\''
-print commit_message
-run_cmd(['git','commit','-m',commit_message])
-run_cmd(['git','push','origin','master'])
+os.chdir(cur_dir)
+err = None
+_,err = run_cmd(['git','add',dir_name])
+commit_message = 'Infra at ' + str(datetime.datetime.now()) 
+_,err = run_cmd(['git','commit','-m',commit_message])
+_,err = run_cmd(['git','push','origin','master'])
+if err:
+  print("Unable to push to the remote repo because of the following error")
+  print err
+
